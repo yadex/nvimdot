4b948d0e9f6152594195b9cabb5da17e4afbda36
yadex <wxn834577101@gmail.com>
2024-08-15 09:33:48 +0800

    upload

--- nvim/lua/plugins/emmet.lua
+++ nvim/lua/plugins/emmet.lua
@@ -1,6 +1,6 @@
 return {
   "olrtg/nvim-emmet",
-  config = function()
-    vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
-  end,
+--  config = function()
+--    vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
+--  end,
 }
