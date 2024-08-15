09c6e1b577e90d18833574de81dbc0319fa96653
yadex <wxn834577101@gmail.com>
2024-08-16 07:18:28 +0800

    upload

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,34 +1,47 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
-				-- You can customize some of the format options for the filetype (:help conform.format)
-				rust = { "rustfmt", lsp_format = "fallback" },
-				-- Conform will run the first available formatter
-				javascript = { "prettier", stop_after_first = true },
-				typescript = { "prettier", stop_after_first = true },
-				javascriptreact = { "prettier", stop_after_first = true },
-				typescriptreact = { "prettier", stop_after_first = true },
-				vue = { "prettier", stop_after_first = true },
-				html = { "prettier", stop_after_first = true },
-				css = { "prettier", stop_after_first = true },
-				scss = { "prettier", stop_after_first = true },
-				less = { "prettier", stop_after_first = true },
-				markdown = { "prettier", stop_after_first = true },
-				json = { "prettier", stop_after_first = true },
-				jsonc = { "prettier", stop_after_first = true },
-				yaml = { "prettier", stop_after_first = true },
-			},
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-		})
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Conform will run multiple formatters sequentially
+			python = { "isort", "black" },
+			-- You can customize some of the format options for the filetype (:help conform.format)
+			rust = { "rustfmt", lsp_format = "fallback" },
+			-- Conform will run the first available formatter
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			vue = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			less = { "prettier" },
+			markdown = { "prettier" },
+			json = { "prettier" },
+			jsonc = { "prettier" },
+			yaml = { "prettier" },
+		},
+		--	format_on_save = {
+		--		-- These options will be passed to conform.format()
+		--		timeout_ms = 500,
+		--		lsp_format = "fallback",
+		--	},
+	},
+
+	--Define a command to run async formatting
+
+	init = function()
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			require("conform").format({ async = true, lsp_format = "fallback", range = range })
+		end, { range = true })
 	end,
 }
