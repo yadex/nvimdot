8b2ee92cec48a01b972c55da84659bcaf511f788
yadex <wxn834577101@gmail.com>
2024-09-06 12:24:29 +0800

    upload

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,12 +1,20 @@
 return {
-	"ibhagwan/fzf-lua",  --build = "./install --bin",
+	"ibhagwan/fzf-lua", --build = "./install --bin",
 	-- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		-- calling `setup` is optional for customization
-		require("fzf-lua").setup({})
-		vim.keymap.set("n", "<c-f>", require("fzf-lua").lgrep_curbuf, { desc = "Fzf Live Grep Current Buffer" })
-		vim.keymap.set("n", "<c-g>", require("fzf-lua").grep_curbuf, { desc = "Fzf Grep Current Buffer" })
+		require("fzf-lua").setup({
+			winopts = {
+				preview = { hidden = "hidden" },
+			},
+		})
+		vim.keymap.set("n", "<c-f>", function()
+			require("fzf-lua").files({ cmd = "rg --color=never --files --hidden --follow  -g '!{.git,.local}/'" })
+		end, { desc = "Fzf Live Grep Current Buffer" })
+
+		vim.keymap.set("n", "<c-g>", require("fzf-lua").lgrep_curbuf, { desc = "Fzf Grep Current Buffer" })
+		-- vim.keymap.set("n", "<c-x><c-g>", require("fzf-lua").grep_curbuf, { desc = "Fzf Grep Current Buffer" })
 		vim.keymap.set("n", "<c-b>", require("fzf-lua").buffers, { desc = "Fzf Buffers" })
 		-- Or, with args
 		--		vim.keymap.set({ "i" }, "<c-x><c-p>",
