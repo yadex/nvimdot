78933facbe9c19b07caeb93f51950d7bae4b62c4
yadex <wxn834577101@gmail.com>
2025-04-22 13:53:37 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,3 +26,16 @@ vim.o.winborder = "rounded"
 if vim.g.neovide then
 	vim.o.guifont = "monospace:h23"
 end
+
+-- noice windownbar
+local initialWinborder = vim.o.winborder
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+	callback = function()
+		vim.o.winborder = "none"
+	end,
+})
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+	callback = function()
+		vim.o.winborder = initialWinborder
+	end,
+})
