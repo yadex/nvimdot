55252a6c287ba7ea5586a4d37966fde800aa6df0
yadex <wxn834577101@gmail.com>
2025-07-28 06:25:18 +0800

    update

---
+++ nvim/plugins/noice.lua
@@ -0,0 +1,37 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		require("notify").setup({
+			background_colour = "#282828",
+		})
+		require("noice").setup({
+			-- disable signature
+			lsp = {
+				signature = {
+					enabled = false,
+				},
+			},
+			views = {
+				cmdline_popup = {
+					position = {
+						row = 5,
+						col = "50%",
+					},
+					size = {
+						width = "50%",
+						height = "auto",
+					},
+				},
+			},
+		})
+	end,
+}
---
+++ nvim/plugins/theme.lua
@@ -0,0 +1,74 @@
+return {
+	{
+		"ellisonleao/gruvbox.nvim",
+		init = function()
+			--override color
+			require("gruvbox").setup({
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = true,
+					emphasis = true,
+					comments = true,
+					operators = false,
+					folds = true,
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_intend_guides = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = "", -- can be "hard", "soft" or empty string
+
+				palette_overrides = {
+					dark1 = "#282828",
+					dark2 = "#282828",
+					bright_red = "#ea6962",
+					bright_green = "#a9b665",
+					bright_yellow = "#d8a657",
+					bright_blue = "#7daea3",
+					bright_purple = "#d3869b",
+					bright_aqua = "#89b482",
+					bright_orange = "#e78a4e",
+				},
+				overrides = {
+					SignColumn = { bg = "#282828" },
+					Pmenu = { bg = "#282828" },
+				},
+			})
+
+			vim.cmd("colorscheme gruvbox")
+			vim.cmd([[highlight Cursorline guibg=#3c3836]])
+		end,
+	},
+
+	-- css-color
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			vim.o.termguicolors = true
+			require("colorizer").setup()
+		end,
+	},
+
+	--Color picker and highlighter plugin for Neovim.
+	----{
+	----	"uga-rosa/ccc.nvim",
+	----	config = function()
+	----		local ccc = require("ccc")
+	----		local mapping = ccc.mapping
+	----
+	----	ccc.setup({
+	----	  -- Your preferred settings
+	----	  -- Example: enable highlighter
+	----	  highlighter = {
+	----	    auto_enable = true,
+	----	    lsp = false,
+	----	  },
+	----	})
+	----	end,
+	----
+	----},
+}
